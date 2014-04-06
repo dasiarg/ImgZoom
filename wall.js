e521b9e861715bb73a49bfb44bed9a2c70243709
Twentyone <gio.balduz@gmail.com>
2014-04-06 09:56:45 +0200

    big update

--- wall.js
+++ wall.js
@@ -1,51 +1,59 @@
 function quit() {
-  	var div= document.getElementById('Wallpaper');
-		div.removeChild(document.getElementById('img'));  //delete img node
-		document.body.removeChild(document.getElementById('canc'));  //delete canc element
-		div.style.width= 0;  //hide div
-		div.style.height= 0;
-	
+    var div = document.getElementById("Wallpaper");
+    div.removeChild(document.getElementById("img"));  //delete img node element
+    div.removeChild(document.getElementById("canc"));  //delete canc node element
+    div.style.width = 0;  //hide div
+    div.style.height = 0;
 }
+
 
 function extend(small) {
-			var x= document.body.clientWidth;  //Needed to resize img
-			var y=  document.body.clientHeight; 
+    var x = Math.max (window.innerWidth,document.body.clientWidth,document.documentElement.clientWidth);
+        y = Math.max (window.innerHeight, document.body.clientHeight,document.documentElement.clientHeight);
+        div = document.getElementById("Wallpaper"),
+        img = document.createElement("img");
+    
+    div.style.width = "100%";
+    div.style.height = "100%";
+    div.style.top = "0px";
+    div.style.left = "0px";
+    
+    img.src = small.src;
+    img.id = "img";
+    
+    //Resizing and centering the image
+    setTimeout(function() {        
+        if (img.width < x && img.height < y) {
+            img.style.marginLeft= (x - img.width)/2 + "px";
+            img.style.marginTop= (y - img.height)/2 + "px";
+        } else if (x/y < img.width/img.height) {
+            newHeight= img.height*(x-30)/img.width;
+            img.height= newHeight;
+            img.width = x-30;
+            img.style.marginLeft= "7px";
+            img.style.marginTop= Math.abs((y - newHeight))/2 + "px";
+        } else if (x/y > img.width/img.height) {
+            newWidth= img.width*(y-30)/img.height;
+            img.width= newWidth;
+            img.height = y-30;
+            img.style.marginTop= "15px";
+            img.style.marginLeft= Math.abs((x - newWidth))/2 + "px";
+        }        
+        div.appendChild(img); 
+    },50);
+    
+    //creating the X icon
+    var canc= document.createElement("div");
+    canc.innerHTML= "x";
+    canc.id= "canc";
+    canc.onclick= function () { quit() };
+    div.insertBefore(canc);
 
-			var div=document.getElementById('Wallpaper'); 
-			div.style.width= "100%";		//show div
-			div.style.height= "100%";
-
-			var img= document.createElement('img');  //create img
-			img.src= small.src;
-			img.id= "img";
-
-			if (img.width/img.height > x/y ) {  //resize img keeping ratio x/y
-				if (img.width > x) {
-					img.height= (img.height * x)/img.width; 
-					img.width= x;
-
-				}				
-			} else {
-				if (img.height > y) {
-					img.width= (img.width * y)/img.height; 
-					img.height= y;
-				}
-			}
-
-			var canc= document.createElement('a');  //create the element to close the img
-			canc.id= "canc";
-			canc.href= "javascript:quit()";
-			canc.innerHTML= "x";			
-
-			document.body.insertBefore(canc, div); //insert in the HTML
-			div.appendChild(img);
-
-			img.style.marginLeft= (x- img.width)/2; //center img
-			img.style.marginTop= (y- img.height)/2;
+       
 }
 
-document.addEventListener('keydown', function(event) { //close img with ESC button
-    if(event.keyCode == 27) {
+document.addEventListener('keydown', function (event) { //close img with ESC button
+    if (event.keyCode === 27) {
         quit();
     }
 });
