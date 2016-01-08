57be047bb7fb7bc091ef92427cd6c049021d6697
Giorgio Balduzzi <gio.balduz@gmail.com>
2016-01-08 12:33:50 +0100

    Very very very better code

--- imgzoom.js
+++ imgzoom.js
@@ -1,73 +1,56 @@
-function quit() {
-    var div = document.getElementById("imgzoom");
-    div.removeChild(document.getElementById("img"));  //delete img node element
-    div.removeChild(document.getElementById("canc"));  //delete canc node element
-    document.body.removeChild(div);
-}
+(function($){
+  function closeOverlay() {
+    $("#overlay").remove();
+  }
 
+  $.fn.imgZoom = function(src){
+    src = src || $(this).attr('src'); //if no src is specified, get the element attr
 
-function extend(link) {
-    var x = Math.max(window.innerWidth, document.body.clientWidth, document.documentElement.clientWidth),
-        y = Math.max(window.innerHeight, document.body.clientHeight, document.documentElement.clientHeight),
-        div = document.createElement("div"),
-        img = document.createElement("img");
-    
-    div.id="imgzoom";
-    div.style.position= "fixed";
-    div.style.backgroundColor="#000";
-    document.body.insertBefore(div);
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    
-    img.src = link;
-    img.id = "img";
-    
-    //Resizing and centering the image
-    img.onload= function() {        
-        if (img.width < x && img.height < y) {
-            img.style.marginLeft= (x - img.width)/2 + "px";
-            img.style.marginTop= (y - img.height)/2 + "px";
-        } else if (x/y < img.width/img.height) {
-            newHeight= img.height*(x-30)/img.width;
-            img.height= newHeight;
-            img.width = x-30;
-            img.style.marginLeft= "7px";
-            img.style.marginTop= Math.abs((y - newHeight))/2 + "px";
-        } else if (x/y > img.width/img.height) {
-            newWidth= img.width*(y-30)/img.height;
-            img.width= newWidth;
-            img.height = y-30;
-            img.style.marginTop= "15px";
-            img.style.marginLeft= Math.abs((x - newWidth))/2 + "px";
-        } 
-        div.appendChild(img);
-    }
-    
-    //creating the X icon
-    var canc= document.createElement("div");
-    canc.style.zIndex= "100";
-    canc.style.position= "fixed";
-    canc.style.fontSize= "1.25em";
-    canc.style.color= "#ccc";
-    canc.style.right= "1em";
-    canc.style.top= "0.5em";
-    canc.style.cursor= "pointer";
-    canc.style.textShadow= "0px 0px 5px #000";
-    canc.innerHTML= "x";
-    canc.id= "canc";
-    canc.onclick= function () { quit() };
-    div.insertBefore(canc, null);       
-    
+    //Overlay creation
+    $("body").append("<div id='overlay'></div>");
+    $("#overlay").css({
+      'position' : 'fixed',
+      'width' : '100%',
+      'height' : '100%',
+      'top' : '0',
+      'left' : '0',
+      'backgroundColor' : 'rgba(0,0,0, 0.8)',
+      'display' : 'flex',
+      'alignItems' : 'center',
+      'justify-content' : 'center;'
+    });
 
-    //font-weight: 500;
+    //Close overlay if clicked
+    $("#overlay").click(function () {
+      closeOverlay();
+    });
 
+    //Image creation and positioning
+    $("#overlay").append("<img src='"+ src +"' id='zoomed_image'/>");
+    $("#zoomed_image").css({
+      'maxWidth' : '95%',
+      'maxHeight' : '95%',
+      'margin' : 'auto'
+    });
 
-}
+    //Don't close overlay if click on img
+    $("#zoomed_image").click(function (e) {
+      return false;
+    });
 
-document.addEventListener('keydown', function (event) { //close img with ESC button
-    if (event.keyCode === 27) {
-        quit();
-    }
-});
+    return this;
+  };
+
+  $(document).ready(function () {
+    $("#IMG").click(function() {
+      $(this).imgZoom();
+    });
+
+    $("body").keydown(function(e) {
+      if(e.keyCode == 27) { // left
+        closeOverlay();
+      }
+    });
+  })
+
+})(jQuery);
